75d431199f07e03d54f24610cee6b8f7a95571f5
olivertoussaint <toussaint.olivier66@gmail.com>
2021-09-18 22:16:42 +0200

    Revert "init"
    This reverts commit 66a58fb387ed0265ecab2440091c8360fed17a9a.

--- script.js
+++
@@ -1,7 +0,0 @@
-const card = $(".perspective__3d");
-
-$('body').on('mousemove', (e) => {
-    let vertical = -($(window).innerWidth() / 300 - e.pageX) / 70, horizontal = ($(window).innerHeight() / 2 - e.pageY) / 70;
-
-    card.attr("style", "transform: rotateY(" + vertical + "deg) rotate(" + horizontal + "deg)")
-});
